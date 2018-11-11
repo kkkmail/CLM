8a889d48aa4f776703fc3cce27542c7acf6920e5
kkkmail <kkk@jfcglobal.net>
2018-11-11 15:28:16 -0500

    20181111.01

--- Clm/Clm/ReactionRates.fs
+++ Clm/Clm/ReactionRates.fs
@@ -8,7 +8,27 @@ open MathNet.Numerics.LinearAlgebra
 open Clm.Substances
 
 module ReactionRates = 
-    let x = 1
+
+    /// Abstraction over all known statistical distrubiton parameters.
+    type DistributionParams =
+        {
+            seed : int option
+        }
+
+    /// Abstraction over statistical distributons.
+    type Distribution (distributionParams : DistributionParams) =
+
+        /// Gives next double from the distrubution.
+        /// It is NOT [0, 1) based but, rather based on the parameters of the distribution.
+        /// This is crucial for long-tailed distributions, which often do not have standard deviation and may not even have mean defined.
+        member dstr.nextDouble() : double = 0.0
+
+        /// Gives next bool from the distribution.
+        member dstr.nextBool() : double = 0.0
+
+    /// Models, which describe catalytic reactions.
+    type CatalystModel = 
+        | X // If substance is a catalyst for some amino acid X, then there are some [separate] chances that it could be a catalyst for other amino acids.
 
     //| ReactionRateProvider of (ReactionInfo -> (ReactionRate option * ReactionRate option))
 
